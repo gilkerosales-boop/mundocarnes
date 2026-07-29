01af8d92f982dda38c1d5c1792102e622a0708d6
gilkerosales-boop <gilkerosales@gmail.com>
2026-07-29 02:22:40 -0400

    Create sw.js

---
+++ factura/sw.js
@@ -0,0 +1,12 @@
+// Service Worker exclusivo para el módulo de facturación
+self.addEventListener('install', (event) => {
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  return self.clients.claim();
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(fetch(event.request));
+});
